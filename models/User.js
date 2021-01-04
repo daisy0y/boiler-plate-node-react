9057344c17b52547b662ce7d12f80733dc294fe4
daisy0y <61371367+daisy0y@users.noreply.github.com>
2021-01-05 00:04:39 +0900

    로그아웃기능

--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+var jwt = require('jsonwebtoken');
+const secretToken = require('../config/token')
 
 const userSchema = mongoose.Schema({
     name:{
@@ -54,6 +56,42 @@ userSchema.pre('save',function(next){
               next()
             }
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    // 입력된 비밀번호와 암호화된 비밀번호를 비교해야함
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    // jsonwebtoken을 이용해 toekn 생성
+    var token = jwt.sign(user._id.toHexString(), secretToken)
+    user.token = token
+    user.save(function(err,user){
+        if(err) return cb(err)
+        cb(null,user)
+    })
+}
+
+userSchema.statics.findByToken = function (token,cb ){
+    var user = this;
+    
+    //  토큰을 descode한다.
+    jwt.verify(token, secretToken,function(err,decoded){
+        // 유저 id를 이용해서 유저를 찾은 다음 
+        //클라이언트에서 가져온 token과 db에 보관된 토큰이 일치하는지 확인
+
+        user.findOne({"_id":decoded,"token": token}, function(err,user){
+
+            if(err) return cb(err);
+            cb(null, user)
+        })
+    })
+}
+
 
 const User = mongoose.model('User', userSchema)
 module.exports = { User }
